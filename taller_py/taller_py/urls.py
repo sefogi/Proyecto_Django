359bd1d4fed873abe15c62f34fb82605e6ea3b9c
sefogi <sebast18@gmail.com>
2024-04-19 03:25:11 +0200

    prueba de rutas y vistas

--- taller_py/taller_py/urls.py
+++ taller_py/taller_py/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from App1.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',IndexView)
 ]
