c3f61713b64e5adf3d8609e2842845cc2fbc9909
sefogi <sebast18@gmail.com>
2024-04-19 03:14:38 +0200

    actualizacion proyecto

--- taller_py/taller_py/settings.py
+++ taller_py/taller_py/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #aplicaciones propias
+    'App1.apps.App1Config'
+    
+    
 ]
 
 MIDDLEWARE = [
