7a7c6bab6216947e542fd8a0223503e85ae5d82a
sefogi <sebast18@gmail.com>
2024-04-22 15:09:46 +0200

    actualizacion 1.0

---
+++ taller_py/App1/static/script.js
@@ -0,0 +1,9 @@
+console.log("hola mundo")
+
+// Declarar varaibles
+
+function suma (a, b) {
+    return a+b;
+}
+
+console.log (suma(5,7));
--- taller_py/App1/tests.py
+++ taller_py/App1/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
 # Create your tests here.
+    # FraseInline can handle a null FraseDb instance
+    def test_handling_null_instance(self):
+        import os
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "your_project.settings")
+        import django
+        django.setup()
+    
+        from django.contrib import admin
+        from .models import FraseDb
+
+        class AutorAdmin(admin.ModelAdmin):
+            fields =["Nombre","Fecha_nacimiento","Fecha_fallecimiento","Profesion","Nacionalidad"]
+            list_display = ["Nombre", "Fecha_nacimiento"]
+            inlines=[FraseInline]
+
+            #crear acciones
+            def actualizar_o(self,request,queryset):
+                for obj in queryset:
+                    if "O" in obj.Nombre:
+                        Nombre1 = obj.Nombre
+                        obj.Nombre = Nombre1.replace("O", "o")
+                        obj.save()  
+                self.message_user(request,"Exitosamente")
+            actualizar_o.short_description="Actualizar letras O"
+            actions=["actualizar_o"]
+
+        class FraseInline(admin.TabularInline):
+            model=FraseDb
+            extra = 1
+
+        frase_inline = FraseInline()
+        assert frase_inline.model == FraseDb
+        assert frase_inline.extra == 1
